02e5e3bf6fabc7f0bac6b33505c38918993bd35c
Juan Carlos Alvarado <vacj.22@gmail.com>
2020-04-06 17:27:44 -0400

    removes swagger config

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,6 +1,3 @@
-/* eslint-disable global-require */
-/* eslint-disable no-restricted-syntax */
-/* eslint-disable import/no-dynamic-require */
 import appRoot from 'app-root-path';
 import express from 'express';
 import fs from 'fs';
@@ -8,35 +5,8 @@ import fs from 'fs';
 import rollbar from '../config/rollbarConfig';
 import expressAuthentication from '../helpers/expressAuthentication';
 
-const { SERVER_HOST, PORT, NODE_ENV } = process.env;
 const router = express.Router();
 const basePath = `${appRoot.path}/src/controllers`;
-const apiDoc = {
-  swagger: '2.0',
-  title: 'Portal Backend Doc.',
-  info: {
-    description:
-      'This documentation gives real examples of how you should consume the Amicar Core API. For this sample, you can use the api key `special-key` to test the authorization filters.',
-    version: '1.2.0',
-    title: 'Portal Backend Doc.',
-    termsOfService: 'https://www.amicar.cl/terminos-y-condiciones',
-    contact: {
-      email: 'ti@amicar.cl',
-    },
-  },
-  host: NODE_ENV !== 'production' ? `${SERVER_HOST}:${PORT}` : SERVER_HOST,
-  basePath: '/v1',
-  schemes: ['https', 'http'],
-  paths: {},
-  securityDefinitions: {
-    api_key: {
-      type: 'apiKey',
-      name: 'x-api-key',
-      in: 'header',
-    },
-  },
-  definitions: {},
-};
 
 module.exports = (dependency) => {
   fs.readdirSync(basePath).forEach((dirName) => {
@@ -44,9 +14,6 @@ module.exports = (dependency) => {
     if (fs.lstatSync(dirPath).isDirectory()) {
       try {
         const ctrlPaths = require(dirPath);
-        const { paths, definitions } = require(`${dirPath}/apiDoc.json`);
-        apiDoc.paths = { ...apiDoc.paths, ...paths };
-        apiDoc.definitions = { ...apiDoc.definitions, ...definitions };
         for (const key in ctrlPaths) {
           if (ctrlPaths.hasOwnProperty(key)) {
             ctrlPaths[key].forEach(({ method, path, ctrl, authenticationRequired }) => {
@@ -62,5 +29,5 @@ module.exports = (dependency) => {
       }
     }
   });
-  return { router, apiDoc };
+  return { router };
 };
