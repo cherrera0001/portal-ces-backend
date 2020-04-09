4a178c6bc2477f4343defbf0f470507bd82b004c
Juan Carlos Alvarado <vacj.22@gmail.com>
2020-04-09 10:37:25 -0400

    adds seed data to mongo db

--- init-mongo.js
+++ init-mongo.js
@@ -1,3 +1,40 @@
+const seedLoans = {
+  loanId: '0065500000FcXi2AAF',
+  financingEntityId: 1,
+  checkList: [
+    {
+      id: '731b4',
+      name: 'Liquidaciones',
+      step: 0,
+      wasSent: false,
+      feError: null,
+      documents: [
+        {
+          id: '8747d',
+          uuid: 'DOCUMENT_UUID',
+        },
+        {
+          id: '87ddd',
+          uuid: 'DOCUMENT_UUID',
+        },
+      ],
+    },
+    {
+      id: '9da13',
+      name: 'Contrato',
+      step: 3,
+      wasSent: true,
+      feError: 'Documento ilegible',
+      documents: [
+        {
+          id: '8345f',
+          uuid: 'DOCUMENT_UUID',
+        },
+      ],
+    },
+  ],
+};
+
 db = db.getSiblingDB('amicar_development');
 
 db.createUser({
@@ -6,6 +43,8 @@ db.createUser({
   roles: [{ role: 'readWrite', db: 'amicar_development' }],
 });
 
+db.loans.insertMany([seedLoans]);
+
 db = db.getSiblingDB('amicar_test');
 
 db.createUser({
@@ -13,3 +52,5 @@ db.createUser({
   pwd: 'amicar',
   roles: [{ role: 'readWrite', db: 'amicar_test' }],
 });
+
+db.loans.insertMany([seedLoans]);
