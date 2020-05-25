96a319480f2b876edb9c1366751ed3e8a400c8a1
Cesar Mayo <cesar.mayo18@gmail.com>
2020-05-25 14:44:32 -0400

    Add getSimulation  endpoint

--- src/graphQL/resolvers/simulation.js
+++ src/graphQL/resolvers/simulation.js
@@ -1,8 +1,9 @@
 import getSimulation from '../../methods/chl/v1/simulation';
+import getCoreParams from "../../methods/chl/v1/getCoreParams";
 
 const resolvers = {
   Query: {
-    getFileUploadingConfig: async (_root, { rollbar }) => getSimulation({ rollbar }),
+    Simulation: async (_root, arg, { rollbar }) => getSimulation({ data: arg, rollbar }),
   },
 };
 
--- src/methods/chl/v1/simulation/index.js
+++ src/methods/chl/v1/simulation/index.js
@@ -1,8 +1,22 @@
 import axios from 'axios';
 
-export default async ({ rollbar }) => {
+export default async ({ data, rollbar }) => {
   try {
-    return 'ok';
+    const headers = {
+      'Content-Type': 'application/json',
+      'x-api-key': 'f2fb29d5831f44598bd634938685186b',
+    };
+    let response = ''
+    await axios
+      .post(`https://api.core.staging.amicar.com/chl/v1/simulation/`, data.simulation, { headers })
+      .then((res) => {
+        console.log(res.data)
+        response = res.data;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    return response;
   } catch (err) {
     rollbar.log(`src/methods/chl/v1/simulation/index::ERROR: ${err.message}`);
     throw new Error(err.message);
