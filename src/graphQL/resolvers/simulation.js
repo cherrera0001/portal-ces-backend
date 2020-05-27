56e49366e7c510f2f812fc76db433abd73f852af
Juan Carlos Alvarado <vacj.22@gmail.com>
2020-05-27 15:40:44 -0400

    adds email endpoint

--- src/graphQL/resolvers/simulation.js
+++ src/graphQL/resolvers/simulation.js
@@ -1,9 +1,12 @@
 import getSimulation from '../../methods/chl/v1/simulation';
-import getCoreParams from "../../methods/chl/v1/getCoreParams";
+import sendEmail from '../../methods/chl/v1/simulation/sendEmail';
 
 const resolvers = {
   Query: {
     Simulation: async (_root, arg, { rollbar }) => getSimulation({ data: arg, rollbar }),
+  },
+  Mutation: {
+    sendEmail: async (_root, arg, { rollbar }) => sendEmail({ data: arg, rollbar }),
   },
 };
 
---
+++ src/methods/chl/v1/simulation/sendEmail.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const { CORE_URL, API_KEY_PORTAL } = process.env;
+
+export default async ({ data, rollbar }) => {
+  try {
+    const headers = {
+      'Content-Type': 'application/json',
+      'x-api-key': API_KEY_PORTAL,
+    };
+    let response = '';
+    await axios
+      .post(`${CORE_URL}/chl/v1/email`, data, { headers })
+      .then((res) => {
+        response = res.data;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    return response;
+  } catch (err) {
+    rollbar.log(`src/methods/chl/v1/simulation/sendEmail::ERROR: ${err.message}`);
+    throw new Error(err.message);
+  }
+};
