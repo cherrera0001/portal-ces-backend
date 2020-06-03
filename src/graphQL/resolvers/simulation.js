5d1c446ca66dc4a7cd46e167bf71844ed5d120f8
Juan Carlos Alvarado <vacj.22@gmail.com>
2020-06-03 12:14:12 -0400

    feature(Simulation): adds getSimulationById query

--- src/graphQL/resolvers/simulation.js
+++ src/graphQL/resolvers/simulation.js
@@ -1,10 +1,12 @@
 import getSimulation from '../../methods/chl/v1/simulation';
+import getSimulationById from '../../methods/chl/v1/simulation/getSimulationById';
 import sendEmail from '../../methods/chl/v1/simulation/sendEmail';
 import saveSimulation from '../../methods/chl/v1/simulation/saveSimulation';
 
 const resolvers = {
   Query: {
     Simulation: async (_root, arg, { rollbar }) => getSimulation({ data: arg, rollbar }),
+    getSimulationById: async (_root, arg, { rollbar }) => getSimulationById({ data: arg, rollbar }),
   },
   Mutation: {
     sendEmail: async (_root, arg, { rollbar }) => sendEmail({ data: arg, rollbar }),
