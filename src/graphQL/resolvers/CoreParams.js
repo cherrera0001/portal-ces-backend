f5dfc2d41d03d711b3a667cb85d582b0584456c6
Juan Carlos Alvarado <vacj.22@gmail.com>
2020-06-09 10:28:21 -0400

    feature(application): adds core params lists query

--- src/graphQL/resolvers/CoreParams.js
+++ src/graphQL/resolvers/CoreParams.js
@@ -1,8 +1,10 @@
 import getCoreParams from '../../methods/chl/v1/getCoreParams';
+import getCoreParamsLists from '../../methods/chl/v1/getCoreParams/lists';
 
 const resolvers = {
   Query: {
     getCoreParams: async (_root, arg, { rollbar }) => getCoreParams({ data: arg, rollbar }),
+    getCoreParamsLists: async (_root, arg, { rollbar }) => getCoreParamsLists({ data: arg, rollbar }),
   },
 };
 
