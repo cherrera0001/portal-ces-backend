9662408c0ddc24497d95431f311c12c81fd474b2
Guillermo Quinteros <gu.quinteros@gmail.com>
2020-06-24 12:11:01 -0400

    Add auction model

--- src/efidoc/controllers/auction.controller.js
+++ src/efidoc/controllers/auction.controller.js
@@ -1,8 +1,21 @@
+const aqp = require('api-query-params');
 const Auction = require('efidoc/models/auction.model');
 
 const all = async (req, res) => {
-  const auctions = await Auction.find();
-  res.json(auctions);
+  const { filter, skip, limit, sort, projection, population } = aqp({ ...req.query });
+  const auctions = await Auction.find(filter)
+    .skip(skip)
+    .limit(limit)
+    .sort(sort)
+    .select(projection)
+    .populate(population);
+
+  const total = await Auction.find(filter).select(projection);
+
+  res.json({
+    total: total.length,
+    result: auctions,
+  });
 };
 
 const create = async (req, res) => {
