afccfc4933da8dee59f4b7100574348df1b44455
Juan Carlos Alvarado <vacj.22@gmail.com>
2020-07-14 17:38:22 -0400

    adds updateCoreParams test and fixes deployments

---
+++ test/0.updateCoreParams.test.js
@@ -0,0 +1,30 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('index');
+
+const { expect } = chai;
+chai.use(chaiHttp);
+
+const getUpdateCoreParamsQuery = () => ({
+  operationName: 'updateCoreParams',
+  query: `
+    query updateCoreParams {
+      updateCoreParams
+    }
+  `,
+});
+
+describe('/POST Updates Core Params before running other tests', () => {
+  it('should return done', (done) => {
+    chai
+      .request(server)
+      .post('/graphql')
+      .send(getUpdateCoreParamsQuery())
+      .end((err, res) => {
+        expect(res.status).to.equal(200);
+        expect(res.body).to.be.a('object');
+        expect(res.body.data.updateCoreParams).to.equal('done');
+        done();
+      });
+  });
+});
