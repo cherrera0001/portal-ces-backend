f16579198fd32b2c57f0a6d08943e18cd00c743e
Juan Carlos Alvarado <vacj.22@gmail.com>
2020-07-23 17:21:28 -0400

    fixes mongo name collision

--- src/debugger.js
+++ src/debugger.js
@@ -3,7 +3,7 @@ const debugModule = require('debug');
 // Set debug namespace for our app
 // https://github.com/visionmedia/debug/issues/117
 exports.debugApp = debugModule('app');
-exports.debugMongo = debugModule('mongo');
+exports.debugMongo = debugModule('ces-database');
 exports.debugRoute = debugModule('route');
 exports.debugPush = debugModule('push');
 exports.debugMail = debugModule('mail');
--- src/portal/subscriptions/simulation.subscription.js
+++ src/portal/subscriptions/simulation.subscription.js
@@ -7,6 +7,63 @@ const formatLoanApplication = (incomeData) => {
     loanType === 'SMART' ? incomeData.amortizationSchedule.find((schedule) => schedule.quotaType === 'SMART') : null;
   const formatedPower = {
     ...incomeData,
+    customer: {
+      ...incomeData.customer,
+      nationality: incomeData.customer.nationality.externalCode,
+      geographicDataId: incomeData.customer.geographicDataId.externalCode,
+      gender: incomeData.customer.gender.externalCode,
+    },
+    customerRequestData: {
+      ...incomeData.customerRequestData,
+      maritalStatus: incomeData.customerRequestData.maritalStatus.externalCode,
+      maritalRegime: incomeData.customerRequestData.maritalRegime.externalCode,
+      academicLevel: incomeData.customerRequestData.academicLevel.externalCode,
+      livingHousehold: incomeData.customerRequestData.livingHousehold.externalCode,
+    },
+    customerActivity: {
+      ...incomeData.customerActivity,
+      workType: incomeData.customerActivity.workType.externalCode,
+      activityTypeId: incomeData.customerActivity.activityTypeId.externalCode,
+      businessSectorId: incomeData.customerActivity.businessSectorId.externalCode,
+      workGeographicDataId: incomeData.customerActivity.workGeographicDataId.externalCode,
+      employmentContractType: incomeData.customerActivity.employmentContractType.externalCode,
+      salaryType: incomeData.customerActivity.salaryType.externalCode,
+    },
+    spouseData: {
+      ...incomeData.spouseData,
+      spouseGeographicDataId: incomeData.spouseData.spouseGeographicDataId.externalCode,
+      workType: incomeData.spouseData.workType.externalCode,
+      activityTypeId: incomeData.spouseData.activityTypeId.externalCode,
+    },
+    buyForAnother: {
+      ...incomeData.buyForAnother,
+      geographicDataId: incomeData.buyForAnother.geographicDataId.externalCode,
+      nationalityId: incomeData.buyForAnother.nationalityId.externalCode,
+      maritalStatus: incomeData.buyForAnother.maritalStatus.externalCode,
+      maritalRegime: incomeData.buyForAnother.maritalRegime.externalCode,
+    },
+    // guarantor: {
+    //   ...incomeData.guarantor,
+    //   geographicDataId: incomeData.guarantor.geographicDataId.externalCode,
+    //   nationalityId: incomeData.guarantor.nationalityId.externalCode,
+    //   maritalStatus: incomeData.guarantor.maritalStatus.externalCode,
+    //   maritalRegime: incomeData.guarantor.maritalRegime.externalCode,
+    //   workType: incomeData.guarantor.workType.externalCode,
+    //   activityTypeId: incomeData.guarantor.activityTypeId.externalCode,
+    // },
+    // bankInformation: {
+    //   ...incomeData.bankInformation,
+    //   codeId: incomeData.bankInformation.codeId.externalCode,
+    // },
+    heritage: {
+      ...incomeData.heritage,
+      // financing: incomeData.heritage ? incomeData.heritage.financing.externalCode,
+      type: incomeData.heritage.type.externalCode,
+    },
+    // personalReferences: {
+    //   ...incomeData.personalReferences,
+    //   type: incomeData.personalReferences.type.externalCode,
+    // },
     loan: {
       ...incomeData.loanSimulationData,
       rateType: incomeData.loanSimulationData.Rate.RateType.cod,
