e9c1725f195e810f2016ad428a2cf1ff0a8b463f
Andres F Santa Maria N <andres@Andress-Mac-Pro.local>
2020-08-06 17:01:43 -0400

    added new method to retrieve checklist by type

--- src/eficar/controllers/auction.controller.js
+++ src/eficar/controllers/auction.controller.js
@@ -1,9 +1,15 @@
 const aqp = require('api-query-params');
+const Params = require('eficar/models/params.model');
 const Auction = require('eficar/models/auction.model');
 const { query } = require('express');
 const { flatMap } = require('lodash');
 const status = {
   DRAFT_SIMULATION: 'No Accesada',
+};
+
+const customerTypeMap = {
+  'R.U.N': 'PJ',
+  'R.U.T': 'PN',
 };
 const all = async (req, res) => {
   const { filter, skip, limit, sort, projection, population } = aqp({ ...req.query });
@@ -40,7 +46,72 @@ const getCustomerHistory = async (req, res) => {
     result: auctions,
   });
 };
+const checklist = async (req, res) => {
+  const { filter, skip, limit, sort, projection, population } = aqp({ ...req.query });
 
+  let stage = req.params.stage;
+  let loanSimulationDataId = req.params.loanSimulationDataId;
+  let loanSimulationData = await Auction.findOne({ 'loanSimulationData.id': loanSimulationDataId })
+    .skip(skip)
+    .limit(limit)
+    .sort(sort)
+    .select(projection)
+    .populate(population);
+
+  let identificationType = await Params.findOne({
+    type: 'IDENTIFICATION_TYPE',
+    id: loanSimulationData.customer.identificationTypeId,
+  })
+    .skip(skip)
+    .limit(limit)
+    .sort(sort)
+    .select(projection)
+    .populate(population);
+
+  let customerTypeCode = customerTypeMap[identificationType.externalCode];
+  let checklistType = await Params.findOne({ type: 'CHECKLIST_TYPE', externalCode: stage })
+    .skip(skip)
+    .limit(limit)
+    .sort(sort)
+    .select(projection)
+    .populate(population);
+
+  let checklistCustomertType = await Params.findOne({
+    type: 'CHECKLIST_CUSTOMER_TYPE',
+    parentId: checklistType.id,
+    externalCode: customerTypeCode,
+  })
+    .skip(skip)
+    .limit(limit)
+    .sort(sort)
+    .select(projection)
+    .populate(population);
+
+  let checklistWorkingType = await Params.findOne({
+    type: 'CHECKLIST_WORK_TYPE',
+    parentId: checklistCustomertType.id,
+    externalCode: loanSimulationData.customerActivity.workType.externalCode,
+  })
+    .skip(skip)
+    .limit(limit)
+    .sort(sort)
+    .select(projection)
+    .populate(population);
+
+  let checklist = await Params.find({
+    type: 'CHECKLIST',
+    parentId: checklistWorkingType.id,
+  })
+    .skip(skip)
+    .limit(limit)
+    .sort(sort)
+    .select(projection)
+    .populate(population);
+
+  res.json({
+    result: checklist,
+  });
+};
 const get = async (req, res) => {
   const { filter, skip, limit, sort, projection, population } = aqp({ ...req.query });
 
@@ -69,4 +140,4 @@ const create = async (req, res) => {
   res.status(201).end();
 };
 
-module.exports = { all, get, create, getCustomerHistory };
+module.exports = { all, get, create, getCustomerHistory, checklist };
--- src/eficar/routes/auction.routes.js
+++ src/eficar/routes/auction.routes.js
@@ -5,6 +5,7 @@ const schema = require('eficar/validations/auction.validation');
 
 router.route('').get(controller.all);
 router.route('/:id').get(controller.get);
+router.route('/checklist/:stage/:loanSimulationDataId').get(controller.checklist);
 router.route('/customerHistory/:rut').get(controller.getCustomerHistory);
 router.route('').post(validate(schema.create), controller.create);
 router.route('/:rut').post(validate(schema.create), controller.create);
