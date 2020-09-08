016d8690a2fd61ed414f0a96ebf051cf73607b8f
Juan Carlos Alvarado <vacj.22@gmail.com>
2020-09-08 16:25:16 -0300

    fixes login token, adds middleware for login requests and responses

--- src/app.js
+++ src/app.js
@@ -5,14 +5,13 @@ const methodOverride = require('method-override');
 const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 const parseSubscriptions = require('middlewares/parseSubscriptions.middleware');
-const logErrors = require('middlewares/logErrors.middleware');
+const saveLogsApi = require('helpers/saveLogsApi');
 
 dotenv.config();
 
 const rollbar = require('./rollbar');
-
-rollbar.log('Server Loaded!');
 
 const app = express();
 app.use(rollbar.errorHandler());
@@ -28,6 +27,26 @@ app.use(
     extended: true,
   }),
 );
+app.use(function(req, res, next) {
+  const oldJson = res.json;
+  req.requestId = mongoose.Types.ObjectId().toHexString();
+  saveLogsApi({
+    url: req.url,
+    method: req.method,
+    requestBody: { ...req.body, ...req.headers },
+    requestId: req.requestId,
+  });
+  res.json = function(data) {
+    saveLogsApi({
+      url: req.url,
+      method: req.method,
+      responseBody: data,
+      requestId: req.requestId,
+    });
+    oldJson.apply(res, arguments);
+  };
+  next();
+});
 app.use(helmet());
 app.use(cors());
 app.use(morgan('tiny'));
@@ -42,7 +61,5 @@ app.get('', (req, res) => {
 app.use('', require('routes'));
 app.use('/amices/chl/v1', require('amices/routes'));
 app.use('/eficar/chl/v1', require('eficar/routes'));
-
-app.use(logErrors);
 
 module.exports = app;
