7e09f885bdc3e12baa0d0d47eb7e98d04f3fd6b6
Juan Carlos Alvarado <vacj.22@gmail.com>
2020-10-29 00:52:18 -0300

    adds params lists endpoint for eficar

--- src/eficar/controllers/params.controller.js
+++ src/eficar/controllers/params.controller.js
@@ -18,6 +18,43 @@ const all = async (req, res) => {
   });
 };
 
+const getQuery = () => {
+  return [
+    { $group: { _id: '$type', values: { $push: '$$ROOT' } } },
+    {
+      $project: {
+        _id: 0,
+        type: '$_id',
+        values: '$values',
+      },
+    },
+  ];
+};
+
+const lists = async (req, res) => {
+  const coreParams = await Params.aggregate(getQuery());
+
+  return res.json({
+    financingType: coreParams.find((el) => el.type === 'FINANCING_TYPE').values,
+    personalReferencesType: coreParams.find((el) => el.type === 'PERSONAL_REFERENCE_TYPE').values,
+    heritageType: coreParams.find((el) => el.type === 'HERITAGE_TYPE').values,
+    bank: coreParams.find((el) => el.type === 'BANKS').values,
+    salaryType: coreParams.find((el) => el.type === 'SALARY_TYPE').values,
+    employmentContractType: coreParams.find((el) => el.type === 'EMPLOYMENT_CONTRACT_TYPE').values,
+    activityType: coreParams.find((el) => el.type === 'ACTIVITY_TYPE').values,
+    workType: coreParams.find((el) => el.type === 'WORK_TYPE').values,
+    maritalStatus: coreParams.find((el) => el.type === 'MARITAL_STATUS').values,
+    maritalRegime: coreParams.find((el) => el.type === 'MARITAL_REGIME').values,
+    academicLevel: coreParams.find((el) => el.type === 'ACADEMIC_LEVEL').values,
+    genderType: coreParams.find((el) => el.type === 'GENDER_TYPE').values,
+    businessSector: coreParams.find((el) => el.type === 'BUSINESS_SECTOR').values,
+    livingHousehold: coreParams.find((el) => el.type === 'LIVING_HOUSEHOLD').values,
+    region: coreParams.find((el) => el.type === 'REGION').values,
+    commune: coreParams.find((el) => el.type === 'COMMUNE').values,
+    nationality: coreParams.find((el) => el.type === 'NATIONALITY').values,
+  });
+};
+
 const getOne = async (filter) => {
   const response = await Params.findOne(filter);
   return response;
@@ -28,4 +65,4 @@ const get = async (filter) => {
   return response;
 };
 
-module.exports = { all, getOne, get };
+module.exports = { all, lists, getOne, get };
